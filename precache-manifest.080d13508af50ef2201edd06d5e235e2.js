c41b09b5d9f2dae64c25a393b917922a5951d5c3
Ciira Kibebe <cik.cir@gmail.com>
2019-07-17 19:42:40 +0300

    Updates

---
+++ precache-manifest.080d13508af50ef2201edd06d5e235e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e330346a554174986b00",
+    "url": "/koothropali-react/static/js/runtime~main.e330346a.js"
+  },
+  {
+    "revision": "01ea84cd41e053e2f24f",
+    "url": "/koothropali-react/static/js/main.01ea84cd.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/koothropali-react/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "01ea84cd41e053e2f24f",
+    "url": "/koothropali-react/static/css/main.77db969f.chunk.css"
+  },
+  {
+    "revision": "72395f9d5816b69ce9a042f619dd9f0d",
+    "url": "/koothropali-react/index.html"
+  }
+];
